b513e676c2074cb96994294944cdfb0c67d5ebe9
sachour <sachour@mit.edu>
2014-08-02 15:11:03 -0400

    grammer now works

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $("#formula_latex").change(function(){
-    	console.log("changed..");
+    	var data = $("#formula_latex").val().substr();
+    	console.log(data);
+    	var structure = parser.parse(data);
+    	var structure_str = JSON.stringify(structure,null,2);
+    	$("#formula_latex_debug").html(structure_str);
     })
 });
