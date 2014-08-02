a7da15628a1b9f1c47eff73f6bb625b7af636816
darkfeffy <afedwin@gmail.com>
2014-08-02 19:25:03 -0400

    renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -5,31 +5,36 @@
 
 //This script moves through a 'tree' dictionary (for now these have been defined in mock.js) and then creates a text output which will then be sent to SVG for display. Function requires a global variable because it is recursive.
 	
-function scan_tree(input)
+function scan_tree(input,parent_node)
 {
-	if (input.left.type == 'op')
+	if (input.left.type == 'op') //Not at a terminal node
 	{
 		left_result = scan_tree(input.left);
+		parent_node = parent_node.data("NODE_ID",node_ID); //Create a parent node and give it an ID (Rapahel)
+
 	}
-	else
+	else //Found an terminal node
 	{
-		left_result = input.left.value;
+		left_result = input.left.value; //Save value in terminal node
+		node_ID = input.left.ID; //Get ID of this node
+		parent_node.data("NODE_ID",node_ID); //Create sub attribute of the most recent parent node and give it an ID(Rapahel)
+
+		//$("formular_render").append()
 	}
 	if (input.right.type == 'op')
 	{
 		right_result = scan_tree(input.right);
+		parent_node = parent_node.data("NODE_ID",node_ID); //Create a parent node and give it an ID (Rapahel)
+
 	}
-	else
+	else //Terminal node on the right
 	{
 		right_result = input.right.value;
+		node_ID = input.right.ID; //Get ID of this node
+		parent_node.data("NODE_ID",node_ID); //Create sub attribute of the most recent parent node and give it an ID(Rapahel)
 	}
+
 	text_result = left_result + ' ' + input.op + ' ' + right_result;
 	return (text_result);
 }
 
-
-
-$("formula_render").attr()
-<svg height="30" width="200">
-  <text x="0" y="15" fill="red">I love SVG!</text>
-</svg>
