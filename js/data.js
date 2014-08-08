5b812e30a235e88c9f1ee334b560eaae6af91a3a
sachour <sachour@mit.edu>
2014-08-08 14:25:18 -0400

    added distribute rule

--- js/data.js
+++ js/data.js
@@ -1,13 +1,16 @@
-
 var Formula = function(data){
+	this._to_match_data = function(d){
+
+	}
 	this.init = function(d){
 		this.data = d;
-	}
-	this.findById = function(id){
-		return {};
+		this.match = this._to_match_data(this.data);
 	}
 	this.toString = function(){
 		return JSON.stringify(this.data,null,2);
+	}
+	this.copy = function(){
+		return JSON.parse(JSON.stringify(this.data,null,2));
 	}
 	this.init(data);
 }
