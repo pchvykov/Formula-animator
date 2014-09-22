a1d38dde6f783b96df920da0f6592adade64b214
sachour <sachour@mit.edu>
2014-09-22 10:44:03 -0400

    added distribution tests

--- test/parser.js
+++ test/parser.js
@@ -5,4 +5,8 @@ QUnit.test( "parser.generic", function( assert ) {
   check_ok('a','a');
   check_ok('a+b','a+b');
   check_ok('\\alpha',"@945");
+  check_ok('a+b+c','a+b+c')
+  check_ok('a+(b+c)+d','a+(b+c)+d')
+  check_ok('a*(b+c)+d','a*(b+c)+d')
+  check_ok('a+(b*c)+d','a+(b*c)+d')
 });
--- test/simpl.js
+++ test/simpl.js
@@ -1,3 +1,14 @@
-QUnit.test( "simpl", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "simpl.distribute", function( assert ) {
+  function test_dist(expr, search, idx, key){
+  	var form = parser.parse(expr);	
+  	var rule = new Transforms.Distribute();
+  	var res = rule.find(search,form);
+  	rule.apply(res[idx],form);
+  	assert.equal(form.print(), key, "dist  with "+search+" on result "+idx+" : "+expr +
+  		 "=>"+form.print() + " (expected: \""+key+"\")");
+  }
+  test_dist('a*(b+c)', '', 0, '(a*(b)+a*(c))');
+  test_dist('(b+c)*a', '', 0, '(a*(b)+a*(c))');
+  test_dist('a*(b+c+d)', '', 0, '(a*(b)+a*(c)+a*(d))');
+  test_dist('(a+q)*(b+c)', '', 0, '((a+q)*(b)+(a+q)*(c))');
 });
