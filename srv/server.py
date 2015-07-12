51869097fd5363ffa6f33ab774e8896851e8e8c4
Zim Ezeozue <zimuzostanley@gmail.com>
2015-07-13 00:57:40 +0100

    Merged servers into one file. Merged html into one file.

--- srv/server.py
+++ srv/server.py
@@ -3,13 +3,78 @@
 import http.server
 import socketserver
 import os
+from urllib.parse import parse_qs
+from cgi import parse_header
+import shutil
+import json
+from subprocess import Popen, PIPE, STDOUT
+
  
-os.chdir('../')
+#os.chdir('../../')
 PORT = 8000
+
+def get_directory(name):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', name))
+
+# Add the corresponding directories on your machine
+SESHAT_PATHS = ['/home/zim/dev/seshat/']
+# Temp just holds temporary files, it can be any empty directory
+TEMP_PATHS = ['/home/zim/dev/temp/']
+base_seshat = ''
+base_temp = ''
+
+for p in SESHAT_PATHS:
+    if os.path.exists(p):
+        base_seshat = p
+        break
+
+for p in TEMP_PATHS:
+    if os.path.exists(p):
+        base_temp = p
+        break
+
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            print(self.path)
+            f = open('index.html', 'r')
+            self.wfile.write(bytes(f.read(), 'UTF-8'))
+        else:
+            f = self.send_head()
+            if f:
+                self.copyfile(f, self.wfile)
+                f.close()
+
+    def do_POST(self):
+        length = int(self.headers['content-length'])
+        data = json.loads(self.rfile.read(length).decode('utf-8'))
+        cmd = base_seshat + 'seshat -c ' + base_seshat + 'Config/CONFIG -i ' + base_temp + 'stroke.scgink'
+        with open(base_temp + 'stroke.scgink', 'w') as f:
+            f.write('SCG_INK\n' + str(len(data)) + '\n')
+            for stroke in data:
+                f.write(str(len(stroke)) + '\n')
+                for e in stroke:
+                    f.write(str(e[0]) + ' ' + str(e[1]) + '\n')
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = p.communicate()
+        print(stderr)
+        if stdout is not None:
+            stdout = stdout.decode('utf-8').split('\n')
+    
+            if stderr.decode('utf-8') is not "":
+                result = {'error': "Error"}
+            elif len(stdout) > 3 and stdout[len(stdout) - 3] == 'LaTeX:':
+                result = {'latex': stdout[len(stdout) - 2]}
+            else:
+                result = {'error': "Error"}
+            result = json.dumps(result)
+        self.wfile.write(bytes(result, 'utf-8'))
+        
  
-Handler = http.server.SimpleHTTPRequestHandler
- 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = http.server.HTTPServer(("", PORT), RequestHandler)
  
 print("serving at port", PORT)
 httpd.serve_forever()
